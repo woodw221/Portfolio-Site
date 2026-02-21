1658c6bd7b935a68b7f93a1196e7ce401a0db4a1
William Wood <woodw221@gmail.com>
2026-02-21 01:09:36 -0600

    changed content to cloudfront domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,7 +23,7 @@ module "aws_cert" {
 resource "cloudflare_record" "portfolio" {
     zone_id = var.cloudflare_zone_id
     name = "@"
-    content = "proud-rock-0b9c6ba0f.6.azurestaticapps.net"
+    content = module.aws.cloudfront_domain
     type = "CNAME"
     proxied = true
     ttl = 1
